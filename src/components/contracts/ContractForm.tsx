5bbfa67cb5c1d3836ec7ec337f173cf4e05cb3b8
CRM Developer <alidduvenci-3385@vercel.com>
2025-11-14 16:30:17 +0300

    Fix: ContractForm useRef import eksikliği düzeltildi

--- src/components/contracts/ContractForm.tsx
+++ src/components/contracts/ContractForm.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { useEffect, useState } from 'react'
+import { useEffect, useState, useRef } from 'react'
 import { useTranslations } from 'next-intl'
 import { useForm } from 'react-hook-form'
 import { zodResolver } from '@hookform/resolvers/zod'
